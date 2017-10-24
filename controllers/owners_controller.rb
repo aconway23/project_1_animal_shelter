b3a9e8d5f5ecf4966033987e5152e8dbb3a2dea5
adam <adamhc23@gmail.com>
2017-10-24 18:07:14 +0100

    owners controllers

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -1,7 +1,29 @@
-
 require_relative('../models/owner.rb')
+
 
 get '/owners' do
   @owners = Owner.all()
+  erb(:"owners/index")
+end
+
+get '/owners/new' do
   erb(:index)
+end
+
+post '/owners' do
+  @owner = Owner.new( params )
+  @owner.save()
+  erb(:create)
+end
+
+post '/owners/:id/delete' do
+  @owner = Owner.find( params[:id].to_i )
+  @owner.delete()
+  erb(:destroy)
+end
+
+post '/owners/:id' do
+  @owner = Owner.new( params )
+  @owner.update()
+  erb(:update)
 end
